86777f6e627d9f4de7baaa42ac723990b0e55721
neootavio <99883591+neootavio@users.noreply.github.com>
2022-08-26 16:07:41 -0300

    ee

---
+++ levelup.js
@@ -0,0 +1,52 @@
+var level = 1;
+var pontosXP = 0;
+var condicao = level + 3;
+var nome = "";
+
+
+function levelUp() {
+
+    if (level < 5) {
+        level++
+        condicao += 3
+    } else if (level < 10) {
+        level++
+        condicao += 4
+    } else if (level < 30) {
+        level++
+        condicao += 7
+    } else {
+        level++
+        condicao += 21
+    }
+
+    alert("Level up!");
+}
+
+function calculaXP(xp) {
+
+    pontosXP += xp
+    console.log(level);
+
+    if (pontosXP > condicao) {
+        levelUp();
+        xp = 0;
+    }
+
+
+    mostraNivel();
+}
+
+function mostraNivel() {
+    alert("Nivel: " + level);
+    alert("pontos XP: " + pontosXP);
+
+}
+
+class Jogador {
+    constructor (nome, lv, xp)
+
+    this.nome = toString(nome)
+    this.level = lv
+    this.experiencia = xp
+}
